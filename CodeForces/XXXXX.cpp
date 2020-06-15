9aa39b0d7cae0ee2c3e27f255b8a100e341e005a
Atul Sharma <2012atulsharma@gmail.com>
2020-06-15 12:05:32 +0530

    Create XXXXX.cpp

---
+++ CodeForces/XXXXX.cpp
@@ -0,0 +1,31 @@
+// FOr probelm statement and its details visit: https://codeforces.com/contest/1364/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while (t--)
+    {
+        int n,x,sum=0,l=-1,r;
+        scanf("%d%d",&n,&x);
+        for (int i=0;i<n;i++)
+        {
+            int a;
+            scanf("%d",&a);
+            if (a%x)
+            {
+                if (l==-1)
+                l=i;
+                r=i;
+            }
+            sum+=a;
+        }
+        if (sum%x)
+        printf("%d\n",n);
+        else if (l==-1)
+        printf("-1\n");
+        else
+        printf("%d\n",n-min(l+1,n-r));
+    }
+}
