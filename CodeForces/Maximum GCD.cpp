b6b5846662218dc22cb115e560e61b78322c5b6a
Atul Sharma <2012atulsharma@gmail.com>
2020-06-21 22:32:03 +0530

    Create Maximum GCD.cpp

---
+++ CodeForces/Maximum GCD.cpp
@@ -0,0 +1,21 @@
+//For problem statement and its details visit: https://codeforces.com/contest/1370/problem/A
+
+#include < bits/stdc++.h >
+using namespace std;
+#define endl "\n"
+#define int long long
+
+const int N = 1e5 + 5;
+int32_t main()
+{
+	IOS;
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		cout << n / 2 << endl;
+	}
+	return 0;
+}
