23d6ae58206c2643859762f3656f614228c79a47
Atul Sharma <2012atulsharma@gmail.com>
2020-06-23 22:22:25 +0530

    Added explanation to the problem

--- CodeForces/AccurateLee.cpp
+++ CodeForces/AccurateLee.cpp
@@ -34,3 +34,6 @@ int main()
     }
     return 0;
     }
+
+// Explanation:If the string s is non-decreasing, then the answer is s itself, otherwise the answer is x+1 zeroes and y ones, where x is the number 
+// of leading zeroes of the string s, and y is the number of trailing ones of the string s.
