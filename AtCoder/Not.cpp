c45f9272dec41136a6c8c3132903f76358353bfa
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2020-09-15 19:14:19 +0530

    Create Not.cpp

---
+++ AtCoder/Not.cpp
@@ -0,0 +1,11 @@
+//Problem Statement:https://atcoder.jp/contests/abc178/tasks/abc178_a
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<(!n)<<'\n';
+  return 0;
+}
