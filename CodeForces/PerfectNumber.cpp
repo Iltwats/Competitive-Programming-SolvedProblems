ae439dcc3b782bb182d3c89d58dcb85d6cacfb03
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2020-10-15 22:59:35 +0530

    Create PerfectNumber.cpp

---
+++ CodeForces/PerfectNumber.cpp
@@ -0,0 +1,25 @@
+// Problem Statement: https://codeforces.com/contest/919/problem/B
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int cal(int num){
+	int ans = 0;
+	while (num){
+		ans += num % 10;
+		num /= 10;
+	}
+	return ans;
+}
+
+int n;
+int main(){
+	scanf("%d", &n);
+	int ans = 0;
+	while (n){
+		ans ++;
+		if (cal(ans) == 10) n--;
+	}
+	printf("%d\n", ans);
+}
