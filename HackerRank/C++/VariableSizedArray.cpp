0954fa2a288321173cef488252d7814210136ca6
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2021-04-28 10:36:35 +0530

    Update VariableSizedArray.cpp

--- HackerRank/C++/VariableSizedArray.cpp
+++ HackerRank/C++/VariableSizedArray.cpp
@@ -21,6 +21,7 @@ int main() {
         for(int j = 0; j < m; j++){
             cin >> o;
             a[i].push_back(o);
+            // for every i, m elements array is inserted into the array i.e a[2]={1,2,3,4} till n here i =2
         }
     }
     
@@ -28,6 +29,7 @@ int main() {
     for(int k = 1; k <= q; k++){
         cin >> r >> s;
         cout << a[r][s] << endl;
+        // then just printing the specific position from array like if r=2, and s=2 from above O/P will be 3.
     }
     return 0;
 
